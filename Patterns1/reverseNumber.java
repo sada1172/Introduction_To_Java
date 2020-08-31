feb10039f3a7e563bbe94cc049530df0a411e4dc
Sadanand Yadav <32041282+sada1172@users.noreply.github.com>
2020-08-31 05:41:52 +0530

    reverseNumber.java

---
+++ Patterns1/reverseNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class reverseNumber {
+
+
+	public static void main(String[] args) {
+         Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i = 1;
+        while(i<=n){
+            int j = 1;
+            int t = i;
+            while(j<=i){
+                System.out.print(t);
+                t--;
+                j++;
+            }
+            System.out.println();
+            i++;
+        }	
+	}
+
+}
